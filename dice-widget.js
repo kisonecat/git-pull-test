bc5a8133e9962234579cec0951174088322f327e
Jim Fowler <fowler@math.osu.edu>
2015-07-27 01:41:36 -0400

    clickable now

--- dice-widget.js
+++ dice-widget.js
@@ -1,13 +1,23 @@
-define(['jquery', 'database'], function(jquery) {
-    var $ = require('jquery');
+define(['jquery', 'database'], function($) {
 
-    var buttonHtml = '<button class="btn btn-primary btn-hint-collapse" type="button" aria-expanded="false" aria-controls="collapse">Roll <span class="dice-roll" style="display: none;">0</span></button>';    
+    var buttonHtml = '<button class="btn btn-primary" type="button">Roll <span class="dice-roll" style="display: none;">0</span></button>';    
     
     $(document).ready( function() {
-	
+
 	$('.dice').each( function(dice) {
 	    var button = $(buttonHtml);
 	    $(dice).append( button );
+
+	    $(dice).persistentData(function(event) {
+		if ('roll' in event.data) {
+		    $(dice).find('.dice-roll').text( event.data.roll );
+		    $(dice).find('.dice-roll').show();
+		}
+	    });
+	    
+	    $(dice).click( function() {
+		$(dice).persistentData('roll', Math.floor(Math.random() * 6) + 1);
+	    });
 	});
 
     });
