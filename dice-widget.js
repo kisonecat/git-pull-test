88b2a72ba7dbfa6231bfbd5a2b07dd9f453dbb11
Jim Fowler <fowler@math.osu.edu>
2015-07-27 01:30:58 -0400

    Button added

--- dice-widget.js
+++ dice-widget.js
@@ -1,8 +1,16 @@
-define(['jquery'], function(jquery) {
+define(['jquery', 'database'], function(jquery) {
     var $ = require('jquery');
+
+    var buttonHtml = '<button class="btn btn-primary btn-hint-collapse" type="button" aria-expanded="false" aria-controls="collapse">Roll <span class="dice-roll" style="display: none;">0</span></button>';    
     
     $(document).ready( function() {
-	console.log( "The document is ready." );
+	
+	$('.dice').each( function(dice) {
+	    var button = $(buttonHtml);
+	    dice.append( button );
+	});
+
     });
 });
+
 
